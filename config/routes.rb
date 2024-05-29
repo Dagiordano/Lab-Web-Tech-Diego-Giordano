4322eee0d7e32161620d62efe5ab79452278dfe5
Diego Giordano <diegogiordano@Diegos-MacBook-Air-2.local>
2024-05-29 17:36:52 -0400

    Feat: profile and log out on nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-  registrations: 'registrations'
-}
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
 
 
 
-  root 'posts#index'
+  root 'static_pages#home'
 
   
   
